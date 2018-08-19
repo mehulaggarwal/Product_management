05c8fca61280034a436b6caa34930aed24f7c14e
mehulaggarwal <mehulaggarwal29@gmail.com>
2018-08-19 10:00:21 +0530

    intial commit

--- server.js
+++ server.js
@@ -8,6 +8,6 @@ app.listen(process.env.PORT||4200);
 //PATH LOCATION STRATEGY
 
 app.get('/*',function(req,res){
-    res.sendFile(Path.JOIN(__dirname+'/dist/welcome.component.html'));
+    res.sendFile(Path.JOIN(__dirname+'/dist/index.html'));
 })
 console.log('Console Listening!');
