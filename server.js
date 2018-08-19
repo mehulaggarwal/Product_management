3eede2fc681950b15cf0358f5fc87b3c61c1fecb
mehulaggarwal <mehulaggarwal29@gmail.com>
2018-08-19 10:07:00 +0530

    first commit

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express=require('express');
 const app=express();
+const path=require('path');
 
 app.use(express.static(__dirname+'/dist'));
 
@@ -8,6 +9,6 @@ app.listen(process.env.PORT||4200);
 //PATH LOCATION STRATEGY
 
 app.get('/*',function(req,res){
-    res.sendFile(Path.JOIN(__dirname+'/dist/index.html'));
+    res.sendFile(path.join(__dirname+'/dist/index.html'));
 })
 console.log('Console Listening!');
